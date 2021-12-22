44915e33b9969500a3438630971b57185814be1a
Luis Palacio <siulacio@hotmail.com>
2021-12-22 14:44:30 -0500

    test: add user-management-test

---
+++ tests/Feature/UserManagementTest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class UserManagementTest extends TestCase
+{
+    //use RefreshDatabase;
+
+    public function test_a_user_can_be_created() : void
+    {
+        $this->withoutExceptionHandling();
+
+        $response = $this->post('/users',[
+            'name' => 'new User',
+            'last_name' => 'new last name',
+            'identification'=>'1234567899',
+            'address' => 'new address',
+            'phone' => '9009009000',
+            'email' => 'newnew@example.com',
+            'email_verified_at' => date('Y-m-s H:m:s'),
+            'role' => 'standar',
+            'password' => 'password',
+        ]);
+
+        $response->assertOk();
+        $user = User::last();
+        $this->assertEquals($user->identification, '1234567899');
+    }
+
+    public function test_get_users() : void
+    {
+        $this->withoutExceptionHandling();
+        $response = $this->get('/users');
+        $response->assertOk();
+        $this->assertCount(3, User::get());
+    }
+}
