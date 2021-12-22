c501326e756972f2ac450fc6d32c895a811abef7
Luis Palacio <siulacio@hotmail.com>
2021-12-22 14:49:04 -0500

    feat: add user controllers methods

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,28 +4,47 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Collection;
 use Illuminate\View\View;
+use App\Http\Requests\UserStoreRequest;
 
 class UserController extends Controller
 {
-    public function edit(int $id) : View
+
+    public function index() : Collection
     {
-        $user = User::find($id);
-        return view('Users.edit_user', compact('user'));
+        return User::get();
     }
 
-    public function update(Request $request) : \Illuminate\Http\RedirectResponse
+    public function store(UserStoreRequest $request) : void
     {
-        $user = User::find($request->id);
-
+        $user = new User();
         $user->name = $request->name;
         $user->last_name = $request->last_name;
+        $user->identification = $request->identification;
         $user->address = $request->address;
         $user->phone = $request->phone;
-
+        $user->role = $request->role;
+        $user->state = $request->state;
+        $user->email = $request->email;
+        $user->password = bcrypt($request->password);
         $user->save();
-
-        return redirect()->route('admin.index')->with('message', 'User updated success');
     }
 
+    public function show(User $user) : User
+    {
+        return $user;
+    }
+
+    public function update(Request $request, User $user) : void
+    {
+        $user->update($request->all());
+    }
+
+    public function changeState(int $id) : void
+    {
+        $user = User::find($id);
+        $user->state = !$user->state;
+        $user->save();
+    }
 }
