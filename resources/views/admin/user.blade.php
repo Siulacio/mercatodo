28ca209ffdf5bf6e686763181bd042d30fe53e4b
Luis Palacio <siulacio@hotmail.com>
2021-12-23 16:05:27 -0500

    feat: user view

---
+++ resources/views/admin/user.blade.php
@@ -0,0 +1,8 @@
+@extends('admin.template')
+@section('title','Administracion Usuarios')
+@section('section',__('Users'))
+@section('content')
+    <div id="app">
+        <user></user>
+    </div>
+@endsection
