83cffa179610b0378f1e7d2f8d955bc94117a51a
Luis Palacio <siulacio@hotmail.com>
2021-12-23 15:56:07 -0500

    test: fix user management test

--- tests/Feature/UserManagementTest.php
+++ tests/Feature/UserManagementTest.php
@@ -9,7 +9,7 @@ use Tests\TestCase;
 
 class UserManagementTest extends TestCase
 {
-    //use RefreshDatabase;
+    use RefreshDatabase;
 
     public function test_a_user_can_be_created() : void
     {
@@ -18,18 +18,19 @@ class UserManagementTest extends TestCase
         $response = $this->post('/users',[
             'name' => 'new User',
             'last_name' => 'new last name',
-            'identification'=>'1234567899',
+            'identification'=>'1234567895',
             'address' => 'new address',
             'phone' => '9009009000',
             'email' => 'newnew@example.com',
             'email_verified_at' => date('Y-m-s H:m:s'),
             'role' => 'standar',
+            'state'=>true,
             'password' => 'password',
         ]);
-
+//        dd($response->json());
         $response->assertOk();
-        $user = User::last();
-        $this->assertEquals($user->identification, '1234567899');
+        $user = User::latest()->first();
+        $this->assertEquals($user->identification, '1234567895');
     }
 
     public function test_get_users() : void
