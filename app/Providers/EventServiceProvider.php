e4f629500d74c1196be56f11170b2a49c7bbe8c4
Luis Palacio <siulacio@hotmail.com>
2022-01-12 08:48:11 -0500

    feat[service provider]: optimize images events and listeners

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -18,6 +18,9 @@ class EventServiceProvider extends ServiceProvider
         Registered::class => [
             SendEmailVerificationNotification::class,
         ],
+        \App\Events\ProductSaved::class =>[
+            \App\Listeners\OptimizeProductImages::class,
+        ],
     ];
 
     /**
