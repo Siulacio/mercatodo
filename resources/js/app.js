631daf2b67bcc471025497485a69feaf0a92b38c
Luis Palacio <siulacio@hotmail.com>
2022-01-14 13:29:30 -0500

    feat: register showcase and product component

--- resources/js/app.js
+++ resources/js/app.js
@@ -11,6 +11,9 @@ Alpine.start();
 window.Vue = require('vue');
 Vue.component('vue-example-name', require('./components/VueExample.vue').default);
 Vue.component('user', require('./components/User.vue').default);
+Vue.component('product', require('./components/Product.vue').default);
+Vue.component('showcase', require('./components/Showcase.vue').default);
+
 const app = new Vue({
     el: '#app',
 });
