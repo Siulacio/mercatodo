1d4440594c046ddee4609b7c3450282f52a8fe2d
Luis Palacio <siulacio@hotmail.com>
2022-01-18 12:15:16 -0500

    feat: product search scope and images relation

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,8 +2,10 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Product extends Model
 {
@@ -19,6 +21,23 @@ class Product extends Model
       'quantity',
       'state'
     ];
+
+    public function scopeSearchProduct($query, ?string $filter) : Builder
+    {
+        if (!empty($filter)){
+            $query->where('code', 'LIKE', '%'.$filter.'%')
+                ->orWhere('price', 'LIKE', '%'.$filter.'%')
+                ->orWhere('name', 'LIKE', '%'.$filter.'%')
+                ->orWhere('description', 'LIKE', '%'.$filter.'%');
+        }
+
+        return $query;
+    }
+
+    public function images(): HasMany
+    {
+        return $this->hasMany(ProductImages::class);
+    }
 
     public function toggleState() : Product
     {
