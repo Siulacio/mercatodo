746960cea384f8d9dc830de7c620af2fb360a3c5
Luis Palacio <siulacio@hotmail.com>
2022-01-27 01:15:24 -0500

    feat: add products export class

--- app/Exports/ProductsExport.php
+++ app/Exports/ProductsExport.php
@@ -4,10 +4,15 @@ namespace App\Exports;
 
 use App\Models\Product;
 use Illuminate\Support\Collection;
+use Maatwebsite\Excel\Concerns\Exportable;
 use Maatwebsite\Excel\Concerns\FromCollection;
+use Illuminate\Contracts\Queue\ShouldQueue;
 
-class ProductsExport implements FromCollection
+
+class ProductsExport implements FromCollection, ShouldQueue
 {
+    use Exportable;
+
     public function collection() : Collection
     {
         return Product::select("code","name","description","price","quantity","state")->get();
